4d23b996962c4bb8b0b119e2d3e5d871a402e0c3
SHAIKH SOHEB AHMED <92992903+soheb21@users.noreply.github.com>
2022-04-04 14:32:50 +0530

    Add files via upload

---
+++ Project-1.py
@@ -0,0 +1,37 @@
+
+from tkinter import * # Graphic k liye use hota hai (tkinter).
+import speedtest 
+
+def speedcheck():
+    sp = speedtest.Speedtest()
+    sp.get_servers()
+    down = str(round(sp.download()/(10**6),3))+"Mbps"
+    up = str(round(sp.upload()/(10**6),3))+"Mbps" 
+    lab_down.config(text=down)
+    lab_up.config(text=up)
+
+sp = Tk()
+sp.title("Internet-SpeedTest")
+sp.geometry("500x500")
+sp.config(bg="Dark Green")
+
+lab = Label(sp,text="Internet Speed Test",font=("Time New Roman",30,"bold"),bg="Dark Green",fg="White")
+lab.place(x=60,y=40,height=50,width=380)
+
+lab = Label(sp,text="Download Speed",font=("Time New Roman",30,"bold"),bg="Dark Green",fg="White")
+lab.place(x=60,y=130,height=50,width=380)
+
+lab_down = Label(sp,text="00",font=("Time New Roman",30,"bold"),bg="Dark Green",fg="White")
+lab_down.place(x=60,y=200,height=50,width=380)
+
+lab= Label(sp,text="Upload Speed",font=("Time New Roman",30,"bold"),bg="Dark Green",fg="White")
+lab.place(x=60,y=290,height=50,width=380)
+ 
+lab_up= Label(sp,text="00",font=("Time New Roman",30,"bold"),bg="Dark Green",fg="White")
+lab_up.place(x=60,y=360,height=50,width=380)
+
+button = Button(sp,text="Check Speed",font=("Time New Roman",30,"bold"),bg="Dark Green",fg="White",relief=RAISED,cursor="Plus",command=speedcheck)
+button.place(x=60,y=440,height=50,width=380)
+
+
+sp.mainloop()
